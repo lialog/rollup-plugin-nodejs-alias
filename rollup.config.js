6383704d4d177cd7522928f049f13c38d9f280c7
lia <milkirre@gmail.com>
2021-12-30 09:22:05 +0900

    fix: fix dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 import cleaner from 'rollup-plugin-cleaner';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default {
 	input: 'src/index.js',
+	preserveModules: true,
 	output: [
 		{
 			format: 'esm',
@@ -17,6 +20,13 @@ export default {
   plugins: [
     cleaner({
       targets: ['./dist']
-    })
+    }),
+		commonjs({
+			sourceMap: false,
+			include: 'node_modules/**',
+		}),
+		resolve({
+			extensions: ['.js'],
+		})
   ]
 };
